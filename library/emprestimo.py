24517d7e80dba1036a61735cc926e47999788cb2
Pancieri <thiagopancieri@gmail.com>
2025-05-11 20:17:03 -0300

    Create emprestimo.py

---
+++ library/emprestimo.py
@@ -0,0 +1,18 @@
+from peewee import CharField, DateTimeField, ForeignKeyField, SQL
+from library.config import ModeloBase
+from library.livro import Livro
+from library.usuario import Usuario
+
+class Emprestimo(ModeloBase):
+    id = CharField(primary_key=True)
+    livro = ForeignKeyField(Livro, backref='emprestimos', on_delete='CASCADE')
+    usuario = ForeignKeyField(Usuario, backref='emprestimos', on_delete='CASCADE')
+    data_emprestimo = DateTimeField(constraints=[SQL('DEFAULT CURRENT_TIMESTAMP')])
+    data_devolucao = DateTimeField(null=True)
+    status = CharField(choices=[
+        ('emprestado', 'Emprestado'),
+        ('devolvido', 'Devolvido')
+    ], default='emprestado')
+
+    def __str__(self):
+        return f"Empréstimo {self.id}: {self.livro.titulo} para {self.usuario.nome} [{self.status}]"
