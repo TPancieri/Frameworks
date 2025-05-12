95245c453eb492debfe0986ddb9b0746acfc0d20
Pancieri <thiagopancieri@gmail.com>
2025-05-11 21:12:16 -0300

    Update crud_funcionarios.py

--- crud_funcionarios.py
+++ crud_funcionarios.py
@@ -1,3 +1,4 @@
+# crud_funcionario.py
 from library import *
 from library.livro import Livro
 from library.cliente import Cliente
@@ -89,4 +90,25 @@ def deletar_cliente():
         print("Cliente deletado com sucesso.")
     except Exception as e:
         print("Erro ao deletar cliente:", e)
+    input("Pressione Enter para continuar.")
+
+def emprestar_livro():
+    print("\n--- EMPRESTAR LIVRO ---")
+    id_usuario = input("ID do cliente: ")
+    isbn = input("ISBN do livro: ")
+    try:
+        Funcionario().registrar_emprestimo(id_usuario, isbn)
+        print("Livro emprestado com sucesso.")
+    except Exception as e:
+        print("Erro ao emprestar livro:", e)
+    input("Pressione Enter para continuar.")
+
+def devolver_livro():
+    print("\n--- DEVOLVER LIVRO ---")
+    id_emprestimo = input("ID do empréstimo: ")
+    try:
+        Funcionario().registrar_devolucao(id_emprestimo)
+        print("Livro devolvido com sucesso.")
+    except Exception as e:
+        print("Erro ao devolver livro:", e)
     input("Pressione Enter para continuar.")
