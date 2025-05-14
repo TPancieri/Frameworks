7610be080870cf9c975443eb6d80f0721144fa2d
Pancieri <thiagopancieri@gmail.com>
2025-05-14 14:58:12 -0300

    Update __init__.py

--- library/__init__.py
+++ library/__init__.py
@@ -1,3 +1,7 @@
+"""
+Exporta os modelos e conexão do db para acesso mais fácil
+"""
+
 from .config import db
 from .livro import Livro
 from .usuario import Usuario
@@ -5,4 +9,5 @@ from .cliente import Cliente
 from .funcionario import Funcionario
 from .emprestimo import Emprestimo
 
+# Define o que é exportado quando usa "from library import *", deixa mais facil de importar
 __all__ = ["db", "Livro", "Usuario", "Cliente", "Funcionario", "Emprestimo"]
